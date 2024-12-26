268861f71ac77fe1ac9775a7c18bb9779194af9e
back2back <lab23vfx@gmail.com>
2024-12-26 15:38:54 +0200

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { ProjectProvider } from './context/ProjectContext'
 import { AuthProvider } from './context/AuthContext'
 import ProtectedRoute from './components/ProtectedRoute'
